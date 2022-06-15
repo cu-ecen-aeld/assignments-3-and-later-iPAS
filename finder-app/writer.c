11cda57d2ea1b0a2e40e4d03b9a739fe7c29bde6
iPAS <ptiwatthanont@gmail.com>
2022-06-16 04:17:42 +0700

    Done writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include <syslog.h>
 
-int main() {
-    openlog(NULL, LOG_NDELAY, LOG_DAEMON);
-    syslog(LOG_DEBUG, "Writing %s to %s", "<str>", "<filename>");
+int main(int argc, char *argv[]) {  // https://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
+    openlog(NULL, LOG_NDELAY, LOG_USER);
+
+    // printf("%d %s %s %s\n", argc, argv[0], argv[1], argv[2]);
+
+    if (argc != 3) {
+        syslog(LOG_ERR, "Two arguments: <str> <filename> are needed!");
+        return 1;
+    }
+
+    const char *str = argv[1];
+    const char *filename = argv[2];
+    FILE *pfile = fopen(filename, "w");
+
+    fwrite(str, strlen(str), 1, pfile);
+    syslog(LOG_DEBUG, "Writing %s to %s", str, filename);
+
+    fclose(pfile);
     closelog();
     return 0;
 }

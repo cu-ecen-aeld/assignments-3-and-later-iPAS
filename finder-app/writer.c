eeba7b4e8b6641e5b6a9e64bdc65f4d97dbf124d
iPAS <ptiwatthanont@gmail.com>
2022-06-16 04:26:30 +0700

    Revise finder-test.sh & fix writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,15 +5,15 @@
 int main(int argc, char *argv[]) {  // https://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
     openlog(NULL, LOG_NDELAY, LOG_USER);
 
-    // printf("%d %s %s %s\n", argc, argv[0], argv[1], argv[2]);
+    printf("%d %s %s %s\n", argc, argv[0], argv[1], argv[2]);
 
     if (argc != 3) {
         syslog(LOG_ERR, "Two arguments: <str> <filename> are needed!");
         return 1;
     }
 
-    const char *str = argv[1];
-    const char *filename = argv[2];
+    const char *str = argv[2];
+    const char *filename = argv[1];
     FILE *pfile = fopen(filename, "w");
 
     fwrite(str, strlen(str), 1, pfile);
